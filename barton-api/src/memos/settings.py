34cae4e93008357f9571a6b7ae62b89ad5330943
Dave Barton <dbarton@svg.agency>
2025-07-09 22:01:49 -0400

    Remove barton-api directory to fix Render deployment confusion

--- barton-api/src/memos/settings.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-
-MEMOS_DIR = Path.cwd() / ".memos"
-DEBUG = False
-
-# "memos" or "memos.submodules" ... to filter logs from specific packages
-LOG_FILTER_TREE_PREFIX = ""
